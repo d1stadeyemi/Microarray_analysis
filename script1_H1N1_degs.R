11f542c5e95068e96d4e33e7a1f80a4ade3c2073
Muhammad Ajagbe <d1stadeyemi@gmail.com>
2025-08-07 12:50:48 -0400

    add figures

--- script1_H1N1_degs.R
+++ script1_H1N1_degs.R
@@ -131,3 +131,73 @@ downregulated_genes <- subset(degs, logFC < -1)
 # dT <- decideTests(fit2, adjust.method = "fdr", p.value = 0.01, lfc = 0)
 
 write.table(degs, file = "DEGs_H1N1.tsv", sep = "\t", quote = FALSE, row.names = FALSE)
+
+# Visualizations and Quality Control
+
+# Plot a histogram to visualize the distribution of Adjusted p-values
+hist(tT$adj.P.Val, col = "grey", border = "white",
+      xlab = "P-adj value", ylab = "Number of genes",
+      main = "P-adj value distribution")
+
+# Filters out genes that failed model fitting (no F-statistic)
+t.good <- which(!is.na(fit2$F))
+
+# Create a quantile-quantile (Q-Q) plot of the moderated t-statistics calculated by limma
+# to assess the distribution and reliability of the test statistics
+# In other words, draw a Q-Q plot to assess if the t-statistics
+# behaved as expected under the null hypothesis
+qqt(fit2$t[t.good], fit2$df.total[t.good], main = "Moderated t statistic")
+
+# Create a volcano plot
+# Ensure required columns exist for the plot
+# by taking the -log10 of the adjusted pvalues
+tT$logP <- -log10(tT$adj.P.Val)
+
+# Plot
+plot(
+     tT$logFC, tT$logP,
+     col="grey", pch=20,
+     xlab="log fold change",
+     ylab="-log10(Adj-pvalue)",
+     main="Volcano Plot of DEGs",
+     )
+
+# Add threshold lines
+abline(h = -log10(0.05), col = "blue", lty = 2)  # p-value threshold
+abline(v = c(-1, 1), col = "red", lty = 2)       # fold change thresholds
+
+# Subset upregulated and downregulated genes
+up <- degs[degs$logFC > 1, ]
+down <- degs[ degs$logFC < -1, ]
+
+# Highlight DEGs
+points(up$logFC, -log10(up$adj.P.Val), col="green", pch=20)
+points(down$logFC, -log10(down$adj.P.Val), col="red", pch=20)
+
+# UMAP plot (dimensionality reduction)
+# Clean the expression data
+ex <- na.omit(ex)                   # Remove missing values
+ex <- ex[!duplicated(ex), ]         # Remove duplicate genes
+
+# Run UMAP (transpose so samples are rows)
+ump <- umap(t(ex), n_neighbors = 15, random_state = 123)
+
+df <- data.frame(UMAP1 = ump$layout[,1],
+                 UMAP2 = ump$layout[,2],
+                 Sample = rownames(ump$layout),
+                 Group = gs)
+
+ggplot(df, aes(x = UMAP1, y = UMAP2, color = Group)) +
+  geom_point(size = 3, alpha = 0.8) +
+  geom_text_repel(aes(label = Sample), size = 3, max.overlaps = 100) +
+  labs(title = "UMAP Plot (n_neighbors = 15)",
+       x = "UMAP 1", y = "UMAP 2") +
+  theme_minimal()
+
+# mean-variance trend, helps to see if precision weights are needed
+plotSA(fit2, main="Mean variance trend, GSE47960")
+
+# expression value distribution
+par(mar=c(4,4,2,1))
+title <- paste ("GSE47960", "/", annotation(gset), " value distribution", sep ="")
+plotDensities(ex, group=gs, main=title, legend ="topright")
